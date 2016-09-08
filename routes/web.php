efd9f322ad143e943bec8656ac7c2ac4e725a437
Adam Williams <adam@whitesunrise.com>
2016-09-08 20:59:48 +0000

    TRPZ Auth Setup

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::get('/home', 'HomeController@index')->middleware('auth');
