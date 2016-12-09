54b40e183a406b24ebeb3040a1f370800b46cbf3
Dustin Weaver <dustin.weaver@payscape.com>
2016-12-08 20:43:15 -0800

    remove hard code activity id from activity more info

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -55,7 +55,7 @@ class FrontendController extends Controller
             $activityDetailsRequest = [
                 'ActivitiesIds'=>[
                     'ActivityId'=>[
-                    'id'=>'1251794'
+                    'id'=>$packageActivity->activity->activityId
                     ]
                 ]
             ];
