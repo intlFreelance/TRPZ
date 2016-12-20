4292394ab1b4d03528e0ce06c3e1938c5ec53275
Dustin Weaver <dustin.weaver@payscape.com>
2016-12-20 11:50:31 -0800

    use correct function for searching hotels

--- app/TouricoHotel.php
+++ app/TouricoHotel.php
@@ -51,7 +51,7 @@ class TouricoHotel extends SoapService {
     }
 
     public function SearchHotels($data){        
-        return $this->call('SearchHotelsById',[$data])->SearchHotelsByIdResult->HotelList;
+        return $this->call('SearchHotels',[$data])->SearchHotelsByIdResult->HotelList;
     }
     public function SearchHotelsById($data){
         return $this->call('SearchHotelsById',[$data])->SearchHotelsByIdResult->HotelList;
