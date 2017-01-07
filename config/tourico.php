dc4bb5ae62160cb0c9408f59d2430911cf71c495
Dustin Weaver <dustin.weaver@payscape.com>
2017-01-06 22:41:34 -0600

    add tourico prod creds

--- config/tourico.php
+++ config/tourico.php
@@ -1,14 +1,23 @@
 <?php
 
 return [
+    // Production
+    'hotel_wsdl'    => 'http://thfwsv3.touricoholidays.com/HotelFlow.svc?wsdl',
+    'hotel_header'  => ['LoginName'=>'Fir106','Password'=>'@p6CxPBU','Culture'=>'en_US','Version'=>'7.123'],
 
-    'hotel_wsdl'    => 'http://demo-hotelws.touricoholidays.com/HotelFlow.svc?WSDL',
-    'hotel_header'  => ['LoginName'=>'Fir110','Password'=>'111111','Culture'=>'en_US','Version'=>'7.123'],
-
-    'activity_wsdl' => 'http://demo-activityws.touricoholidays.com/ActivityBookFlow.svc?wsdl',
-    'activity_header' => ['LoginName'=>'fir123','Password'=>'111111','Culture'=>'en_US','Version'=>'7.123'],
+    'activity_wsdl' => 'http://activityws.touricoholidays.com/ActivityBookFlow.svc?wsdl',
+    'activity_header' => ['LoginName'=>'Fir106','Password'=>'@p6CxPBU','Culture'=>'en_US','Version'=>'7.123'],
 
     'destination_wsdl' => 'http://destservices.touricoholidays.com/DestinationsService.svc?wsdl',
-    'destination_header' => ['username'=>'destinfirst','password'=>'Destination@18','culture'=>'en_US','version'=>'7.123']
+    'destination_header' => ['username'=>'Fir106','password'=>'@p6CxPBU','culture'=>'en_US','version'=>'7.123']
 
+    // Demo
+    // 'hotel_wsdl'    => 'http://demo-hotelws.touricoholidays.com/HotelFlow.svc?WSDL',
+    // 'hotel_header'  => ['LoginName'=>'Fir110','Password'=>'111111','Culture'=>'en_US','Version'=>'7.123'],
+
+    // 'activity_wsdl' => 'http://demo-activityws.touricoholidays.com/ActivityBookFlow.svc?wsdl',
+    // 'activity_header' => ['LoginName'=>'fir123','Password'=>'111111','Culture'=>'en_US','Version'=>'7.123'],
+
+    // 'destination_wsdl' => 'http://destservices.touricoholidays.com/DestinationsService.svc?wsdl',
+    // 'destination_header' => ['username'=>'destinfirst','password'=>'Destination@18','culture'=>'en_US','version'=>'7.123']
 ];
