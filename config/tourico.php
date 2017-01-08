68aeca6d6955adc8422326e515dc9076e1ce8986
Dustin Weaver <dustin.weaver@payscape.com>
2017-01-07 18:23:14 -0600

    keep prod creds for admin, demo creds for frontend

--- config/tourico.php
+++ config/tourico.php
@@ -1,6 +1,6 @@
 <?php
 
-return [
+$production = [
     // Production
     'hotel_wsdl'    => 'http://thfwsv3.touricoholidays.com/HotelFlow.svc?wsdl',
     'hotel_header'  => ['LoginName'=>'Fir106','Password'=>'@p6CxPBU','Culture'=>'en_US','Version'=>'7.123'],
@@ -9,15 +9,28 @@ return [
     'activity_header' => ['LoginName'=>'Fir106','Password'=>'@p6CxPBU','Culture'=>'en_US','Version'=>'7.123'],
 
     'destination_wsdl' => 'http://destservices.touricoholidays.com/DestinationsService.svc?wsdl',
-    'destination_header' => ['username'=>'Fir106','password'=>'@p6CxPBU','culture'=>'en_US','version'=>'7.123']
-
-    // Demo
-    // 'hotel_wsdl'    => 'http://demo-hotelws.touricoholidays.com/HotelFlow.svc?WSDL',
-    // 'hotel_header'  => ['LoginName'=>'Fir110','Password'=>'111111','Culture'=>'en_US','Version'=>'7.123'],
-
-    // 'activity_wsdl' => 'http://demo-activityws.touricoholidays.com/ActivityBookFlow.svc?wsdl',
-    // 'activity_header' => ['LoginName'=>'fir123','Password'=>'111111','Culture'=>'en_US','Version'=>'7.123'],
-
-    // 'destination_wsdl' => 'http://destservices.touricoholidays.com/DestinationsService.svc?wsdl',
-    // 'destination_header' => ['username'=>'destinfirst','password'=>'Destination@18','culture'=>'en_US','version'=>'7.123']
+    'destination_header' => ['username'=>'Fir106','password'=>'@p6CxPBU','culture'=>'en_US','version'=>'7.123'],
 ];
+
+$demo = [
+    // Demo
+    'hotel_wsdl'    => 'http://demo-hotelws.touricoholidays.com/HotelFlow.svc?WSDL',
+    'hotel_header'  => ['LoginName'=>'Fir110','Password'=>'111111','Culture'=>'en_US','Version'=>'7.123'],
+
+    'activity_wsdl' => 'http://demo-activityws.touricoholidays.com/ActivityBookFlow.svc?wsdl',
+    'activity_header' => ['LoginName'=>'fir123','Password'=>'111111','Culture'=>'en_US','Version'=>'7.123'],
+
+    'destination_wsdl' => 'http://destservices.touricoholidays.com/DestinationsService.svc?wsdl',
+    'destination_header' => ['username'=>'destinfirst','password'=>'Destination@18','culture'=>'en_US','version'=>'7.123']
+];
+
+$uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri_segments = explode('/', trim($uri_path, '/'));
+
+if($uri_segments[0] == 'admin') {
+    return $production;
+}
+
+return $demo;
+
+
