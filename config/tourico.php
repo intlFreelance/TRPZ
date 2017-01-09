4e3061e39d45e88f83bd370cfe0629dac34fe993
Dustin Weaver <dustin.weaver@payscape.com>
2017-01-09 09:43:00 -0600

    use tourico prod creds throughout

--- config/tourico.php
+++ config/tourico.php
@@ -24,6 +24,9 @@ $demo = [
     'destination_header' => ['username'=>'destinfirst','password'=>'Destination@18','culture'=>'en_US','version'=>'7.123']
 ];
 
+// Returning Production only for the test sever
+return $production;
+
 $uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri_segments = explode('/', trim($uri_path, '/'));
 
